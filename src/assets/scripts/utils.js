651e828d2912244fc510d249180b6f0bae5fb229
loickcherimont <loickcherimont@gmail.com>
2023-12-22 11:10:33 +0100

    Create sanitize function for user input

---
+++ src/assets/scripts/utils.js
@@ -0,0 +1,5 @@
+export function getAlphabet() {
+    const alpha = Array.from(Array(26)).map((_, index) => index + 65)
+    const alphabet = alpha.map((x) => String.fromCharCode(x))
+    return alphabet
+}
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,82 @@
+import { useState } from "react"
+import { getAlphabet } from "../assets/scripts/utils"
+
 export function Form() {
-    return <form>
+
+    const [letters, setLetters] = useState("")
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        sanitize(letters)
+
+        // e.target.reset() // to empty fields
+
+        // const arrayLetters = letters.split("")
+
+        // console.log(arrayLetters)
+    }
+
+    // Update the input value
+    // When user writes
+    const handleChange = (e) => {
+        setLetters(e.target.value)
+    }
+
+    return <form onSubmit={handleSubmit} id="form" autoComplete="off">
         <h2>Enter some letter and discover all words possibles to write with them</h2>
-        <label for="words" className="flex text-sm font-medium leading-6 text-gray-900">
-            <input type="text" id="words" name="words" className="border border-gray-400" />
+        <label htmlFor="words" className="flex text-sm font-medium leading-6 text-gray-900">
+            <input
+                type="text"
+                id="words"
+                name="words"
+                className="border border-gray-400"
+                value={letters ?? ""}
+                onChange={handleChange} />
             <button type="submit" className="rounded-r-lg bg-cyan-500 p-2">Check Words</button>
         </label>
     </form>
 }
+
+// Verify if user input is valid
+function sanitize(inputValue) {
+
+    const ALPHABET = getAlphabet()
+
+    inputValue = inputValue.toUpperCase()
+
+    // Not empty
+    if (!inputValue.trim()) {
+        console.log("Please complete this input!")
+        return
+    }
+
+    // All characters are included in alphabet
+    try {
+        compare(inputValue, ALPHABET)
+    } catch (e) {
+        console.error(e)
+        return
+    }
+
+    // clean entry
+    console.log(inputValue)
+}
+
+// Verify if all character in word are included in reference
+function compare(word, reference) {
+    const [...rest] = word // work with an array instead of a string
+
+    rest.forEach(character => {
+
+
+        if (reference.indexOf(character) == -1) {
+            throw new Error(`This entry is not character is not valid: ${character}`)
+        }
+    })
+
+    console.log("That's great!", word)
+}
+
+
+
